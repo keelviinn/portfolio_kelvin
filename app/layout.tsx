d0dc651fe138f2fa58119d7511fff843ccaa643f
Kelvin Oliveira <keelviinn@gmail.com>
2022-10-31 21:52:38 +0000

    themeButton refact

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,15 @@
-import { Navigation } from '../components/Navigation';
-import { navItems } from '../variables/navItems';
+/* eslint-disable @next/next/no-page-custom-font */
+"use client";
+
+import { ThemeProvider } from "next-themes";
+import { Navigation } from "../components/Navigation";
+import { navItems } from "../variables/navItems";
 
 import "../styles/globals.css";
 
-export default function RootLayout({ children }: {
+export default function RootLayout({
+  children,
+}: {
   children: React.ReactNode;
 }) {
   return (
@@ -14,15 +20,22 @@ export default function RootLayout({ children }: {
         <meta charSet="utf-8" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
       </head>
-      <div className='min-h-screen flex flex-col'>
-        <Navigation navItems={navItems} />
-        <main className='h-screen pt-32'>
-          {children}
-        </main>
-      </div>
+      <ThemeProvider attribute="class">
+        <div className="min-h-screen flex flex-col">
+          <Navigation navItems={navItems} />
+          <main className="h-screen pt-32">{children}</main>
+        </div>
+      </ThemeProvider>
     </html>
   );
 }
